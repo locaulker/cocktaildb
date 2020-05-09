bfaa7ad2d079c8fa331c5b3c08cc165dbfb09462
Lawrence <Caulker>
2020-05-09 12:31:31 -0400

    installed and setup react-router-dom

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -1,7 +1,10 @@
 import React from "react"
+import { useParams } from "react-router-dom"
 
 const SingleCocktail = () => {
-  return <h1>single cocktail page</h1>
+  const { id } = useParams()
+
+  return <h1>single cocktail page id: {id}</h1>
 }
 
 export default SingleCocktail
