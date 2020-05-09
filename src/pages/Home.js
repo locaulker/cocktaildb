362850a1c6add4fd42efc5e99c1cce89eff8a285
Lawrence <Caulker>
2020-05-09 13:36:46 -0400

    setup basic home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,18 @@
-import React from "react"
+import React, { useState } from "react"
+import CocktailList from "../components/CocktailList"
+import SearchForm from "../components/SearchForm"
 
 const Home = () => {
-  return <h1>Home page</h1>
+  const [loading, setLoading] = useState(false)
+  const [searchTerm, setSearchTerm] = useState("s")
+  const [cocktails, setCocktails] = useState([])
+
+  return (
+    <main>
+      <SearchForm setSearchTerm={setSearchTerm} />
+      <CocktailList loading={loading} cocktails={cocktails} />
+    </main>
+  )
 }
 
 export default Home
