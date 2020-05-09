e580dc310d20a192378be4e1eefc35775be41a64
Lawrence <Caulker>
2020-05-09 13:23:26 -0400

    addedd error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,17 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 const Error = () => {
-  return <h1>error page</h1>
+  return (
+    <section className="error-page section">
+      <div className="error-container">
+        <h1>Oops! No page found!</h1>
+        <Link to="/" className="btn btn-primary">
+          Back to HomePage
+        </Link>
+      </div>
+    </section>
+  )
 }
 
 export default Error
