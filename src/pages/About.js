d10f9d660d0630e1dd061fccb646d4c5f8665651
Lawrence <Caulker>
2020-05-09 13:05:53 -0400

    addedd about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,38 @@
 import React from "react"
 
 const About = () => {
-  return <h1>about page</h1>
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        I'm baby street art chillwave small batch church-key taiyaki cliche
+        fanny pack DIY mlkshk kombucha you probably haven't heard of them
+        humblebrag bitters. Pickled unicorn humblebrag, post-ironic
+        intelligentsia williamsburg hella crucifix. Lo-fi hot chicken selvage
+        banh mi organic pok pok keffiyeh small batch raclette iPhone snackwave
+        post-ironic. DIY small batch blog heirloom lomo.
+      </p>
+      <p>
+        Meh viral lumbersexual irony lomo marfa beard kogi scenester, YOLO retro
+        disrupt master cleanse. Chartreuse plaid shabby chic, live-edge hot
+        chicken vaporware narwhal ethical. Everyday carry man bun PBR&B iceland
+        fam mlkshk. Chillwave literally edison bulb, brunch shoreditch semiotics
+        polaroid schlitz fanny pack crucifix 3 wolf moon raclette. PBR&B fanny
+        pack pok pok, cornhole intelligentsia plaid heirloom jianbing cardigan
+        fingerstache. Tbh mixtape gastropub, health goth kale chips poke marfa
+        heirloom DIY. Meggings tousled narwhal, fingerstache flexitarian
+        helvetica microdosing franzen truffaut unicorn adaptogen.
+      </p>
+      <p>
+        Four loko unicorn yr VHS ethical. Meggings selfies austin, mustache
+        coloring book poutine thundercats retro seitan yuccie. Fashion axe meh
+        glossier affogato, humblebrag plaid vape vegan tofu. Taxidermy squid
+        flexitarian jianbing everyday carry yr art party wayfarers blog lo-fi
+        chia. Crucifix sriracha prism kale chips farm-to-table keytar disrupt
+        fanny pack. Next level air plant bespoke, truffaut etsy keytar twee.
+      </p>
+    </section>
+  )
 }
 
 export default About
