92b0871a1a960f17c47e21bd2316bbc67fd0d3cd
Lawrence <Caulker>
2020-05-10 01:39:29 -0400

    setup singleCocktail details functionality

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -1,8 +1,63 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
 
 const SingleCocktail = () => {
   const { id } = useParams()
+  const [loading, setLoading] = useState(false)
+  const [cocktail, setCocktail] = useState(null)
+
+  useEffect(() => {
+    setLoading(true)
+    const getCocktail = async () => {
+      try {
+        const response = await fetch(
+          `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
+        )
+        const data = await response.json()
+
+        if (data.drinks) {
+          const {
+            strDrinks: name,
+            strDrinkThumb: image,
+            strAlcoholic: info,
+            strDrinkCategory: category,
+            strGlass: glass,
+            strInstructions: instructions,
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5,
+          } = data.drinks[0]
+
+          const ingredients = [
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5,
+          ]
+
+          const newCocktail = {
+            name,
+            image,
+            info,
+            category,
+            glass,
+            instructions,
+            ingredients,
+          }
+          setCocktail(newCocktail)
+        } else {
+          setCocktail(null)
+        }
+      } catch (error) {
+        console.log(error)
+      }
+      setLoading(false)
+    }
+    getCocktail()
+  }, [id])
 
   return <h1>single cocktail page id: {id}</h1>
 }
