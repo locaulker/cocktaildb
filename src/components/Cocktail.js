f44d6be9f8473abccb9c5741a85fd204aca0a2cf
Lawrence <Caulker>
2020-05-10 00:38:04 -0400

    setup useEffect and cocktailList component

--- src/components/Cocktail.js
+++ src/components/Cocktail.js
@@ -1,7 +1,22 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
-const Cocktail = () => {
-  return <h1>cocktail component</h1>
+const Cocktail = ({ image, name, id, info, glass }) => {
+  return (
+    <article className="cocktail">
+      <img src={image} alt={name} />
+      <div className="cocktail-footer">
+        <h3>
+          <span>Cocktail Name:</span> {name}
+        </h3>
+        <h4>{glass}</h4>
+        <p>{info}</p>
+        <Link to={`/cocktail/${id}`} className="btn btn-primary">
+          Get Details
+        </Link>
+      </div>
+    </article>
+  )
 }
 
 export default Cocktail
--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -1,7 +1,25 @@
 import React from "react"
+import Cocktail from "./Cocktail"
 
-const CocktailList = () => {
-  return <h1>cocktailList component</h1>
+const CocktailList = ({ cocktails, loading }) => {
+  if (loading) {
+    return <h2 className="section-title">Loading...</h2>
+  }
+
+  if (cocktails.length < 1) {
+    return <h2 className="section-title">No matching results</h2>
+  }
+
+  return (
+    <section className="section">
+      <h2 className="section-title">Cocktails</h2>
+      <div className="cocktails-center">
+        {cocktails.map(item => {
+          return <Cocktail key={item.id} {...item} />
+        })}
+      </div>
+    </section>
+  )
 }
 
 export default CocktailList
